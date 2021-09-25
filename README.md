c1d562afb96d7edd070f2d51ae6ed2fb8b661d9c
Di <altusj@gmail.com>
2021-09-24 21:03:00 -0400

    backend folder and API test

--- README.md
+++ README.md
@@ -2,5 +2,16 @@
 
 Capstone project for Per Scholas React Course
 
+MongoDB
+ExpressJS
+React
+NodeJS
+
+Rouiting:
+‣ react-router-dom
+‣ react-router-bootstrap
+
 UI:
-React-Bootstrap with bootswatch.com (Free themes for Bootstrap).
+‣ React-Bootstrap
+‣ bootswatch.com (Free themes for Bootstrap).
+‣ font-awesome
---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Photoshop',
+    image: '/images/ps.png',
+    description:
+      "Welcome to The Complete Adobe Photoshop Mastery Course for Beginners, where you'll learn the basics of Adobe Photoshop in a fun, hands-on way. This course is designed to teach you the most important elements of beginning Photoshop without the frustration or stress that often comes with learning new software.",
+    brand: 'Adobe',
+    category: 'Courses',
+    price: 8.99,
+    countInStock: 10,
+    rating: 4.1,
+    numReviews: 25,
+  },
+  {
+    _id: '2',
+    name: 'Illustrator',
+    image: '/images/il.png',
+    description:
+      "Because it works seamlessly with Photoshop, Adobe Illustrator has become a graphic design staple. But even if you aren’t working as a designer, you can still benefit from Udemy’s Illustrator courses. For instance, you can learn how to design your personal website or a new business logo.",
+    brand: 'Adobe',
+    category: 'Courses',
+    price: 7.99,
+    countInStock: 7,
+    rating: 4.2,
+    numReviews: 9,
+  },
+  {
+    _id: '3',
+    name: 'Java',
+    image: '/images/java.png',
+    description:
+      "You’ve just stumbled upon the most complete, in-depth Java programming course online. With close to 600,000 students enrolled and over one hundred and forty thousand reviews (with tens of thousands of those 5-star) to date, these comprehensive java tutorials cover everything you’ll ever need.",
+    brand: 'Oracle',
+    category: 'Courses',
+    price: 0.99,
+    countInStock: 5,
+    rating: 5,
+    numReviews: 112,
+  },
+  {
+    _id: '4',
+    name: 'JavaScript',
+    image: '/images/js.png',
+    description:
+      "Javascript is the language that modern developers need to know, and know well. Truly knowing Javascript will get you a job, and enable you to build quality web and server applications.",
+    brand: 'ECMA',
+    category: 'Courses',
+    price: 5.99,
+    countInStock: 11,
+    rating: 3,
+    numReviews: 22,
+  },
+  {
+    _id: '5',
+    name: 'Python',
+    image: '/images/py.png',
+    description:
+      "The Python Mega Course is the most practical course you will find on the web today. In this course, rather than practicing rote memorization, students are actively problem-solving towards tangible goals. You will learn to build real-life desktop programs with graphical user interfaces,  automate routine tasks, control the computer webcam, develop interactive web applications using Flask and Django, scrape and visualize data, and more.",
+    brand: 'Python SW Foundation',
+    category: 'Courses',
+    price: 9.99,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 100,
+  },
+  {
+    _id: '6',
+    name: 'Ruby',
+    image: '/images/ruby.png',
+    description:
+      "Since its introduction, Ruby on Rails has rapidly become one of the most popular and powerful web application development tools for both startups and mature software companies. Some of the top sites in the world started with Ruby on Rails such as Basecamp, Twitter, Shopify, Github, LivingSocial, Groupon, Hulu, Airbnb, Yellow Pages and many more!",
+    brand: 'Ruby-Lang',
+    category: 'Courses',
+    price: 6.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
